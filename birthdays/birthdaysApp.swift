3711bf42bd6ff34269f5262535dfd58bcb29c05a
Evie <Samantha.Fisher123@gmail.com>
2025-06-06 10:22:06 -0500

    Initial Commit

---
+++ birthdays/birthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  birthdaysApp.swift
+//  birthdays
+//
+//  Created by Scholar on 6/6/25.
+//
+
+import SwiftUI
+
+@main
+struct birthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
