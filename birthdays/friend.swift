09d28768c909c56b3aeb716d02eb28dc419fd352
Evie <Samantha.Fisher123@gmail.com>
2025-06-06 11:45:09 -0500

    Finished step 3

---
+++ birthdays/friend.swift
@@ -0,0 +1,19 @@
+//
+//  friend.swift
+//  birthdays
+//
+//  Created by Scholar on 6/6/25.
+//
+
+import Foundation
+import Foundation
+
+class Friend {
+    var name: String
+    var birthday: Date
+    
+    init(name: String, birthday: Date) {
+        self.name = name
+        self.birthday = birthday
+    }
+}
